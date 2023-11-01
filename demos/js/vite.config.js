a413ddadd0856854caa625694fbda9303d23aa81
Luca Invernizzi <invernizzi.l@gmail.com>
2023-11-01 10:50:43 +0000

    Test importing retvec from a different directory

--- demos/js/vite.config.js
+++ demos/js/vite.config.js
@@ -3,4 +3,10 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+
+  build: {
+    rollupOptions: {
+      external: ['@tensorflow/tfjs/dist/tf.fesm.js'],
+    },
+  },
 });
