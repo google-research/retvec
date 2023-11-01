b532cf087050808b24849c85374457dbea1d5f92
Luca Invernizzi <invernizzi.l@gmail.com>
2023-11-01 11:11:51 +0000

    Remove hashes from asset urls.

--- demos/js/vite.config.js
+++ demos/js/vite.config.js
@@ -3,4 +3,15 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+
+  build: {
+    rollupOptions: {
+      output: {
+        // Do not add hashes to the url of asset files.
+        // This is important as TFJS loads the weights of the model
+        // based on its url.
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
 });
