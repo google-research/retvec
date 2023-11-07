94449dbb1aea6592e8b606cf4a0adeb8e4253e2c
Luca Invernizzi <invernizzi.l@gmail.com>
2023-11-07 21:58:35 +0000

    Vue router and github pages

---
+++ demos/js/src/components/Homepage.vue
@@ -0,0 +1,9 @@
+<template>
+      <h1>Hello App!</h1>
+  <p>
+    <router-link to="/binarizer_demo">Go to binarizer</router-link>
+   </p>
+   <p>
+    <router-link to="/emotion_demo">Go to emotion</router-link>
+  </p>
+</template>
--- demos/js/src/main.js
+++ demos/js/src/main.js
@@ -2,16 +2,17 @@ import { createApp } from "vue";
 import {createRouter, createWebHistory} from 'vue-router';
 
 import App from "./App.vue";
+import Homepage from "./components/Homepage.vue";
 import BinarizerDemo from "./components/BinarizerDemo.vue";
 import EmotionDemo from "./components/EmotionDemo.vue";
 
 // Load each demo on a separate route.
 const router = createRouter( {
-    history: createWebHistory(),
+    history: createWebHistory('/retvec/'),
     routes: [
-        { path: '/', name: "Default", redirect: { name: 'Home' }},
-        {path: '/retvec/', name: 'Home', component: BinarizerDemo},
-        {path: '/retvec/emotion_demo', name: 'EmotionDemo', component: EmotionDemo},
+        {path: '/', name: 'Home', component: Homepage},
+        {path: '/binarizer_demo', name: 'BinarizerDemo', component: BinarizerDemo},
+        {path: '/emotion_demo', name: 'EmotionDemo', component: EmotionDemo},
     ]
 });
 
