5dde0faa457470571933b3dfea4408e0f2c863ac
Luca Invernizzi <invernizzi.l@gmail.com>
2023-11-07 21:38:01 +0000

    Emotion demo - first steps

---
+++ demos/js/src/components/EmotionDemo.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="status-message">{{ message }}</div>
+  <input
+    v-model="userInput"
+    placeholder="Type a word to see its binarized version"
+  />
+  <h2>Verdict</h2>
+  <div>{{ verdict }}</div>
+  <h2> Full output</h2>
+  <pre>{{ fullOutput }}</pre>
+</template>
+
+<script setup>
+import { onMounted, ref, watch } from "vue";
+import * as tf from "@tensorflow/tfjs";
+import RetVec from "../../retvecjs/retvec.ts";
+import debounce from "lodash/debounce";
+
+// We import the model here to make sure that it's available in a production build.
+import retvecModelUrl from "../../retvecjs/model/v1/model.json?url";
+import retvecModelWeightsUrl from "../../retvecjs/model/v1/group1-shard1of1.bin?url";
+import modelUrl from "../../../../notebooks/demo_models/emotion_model_tfjs_converted/model.json?url";
+import modelWeightsUrl from "../../../../notebooks/demo_models/emotion_model_tfjs_converted/group1-shard1of1.bin?url";
+
+const RETVEC_ENCODING_SIZE = 256;
+const RETVEC_STR_LENGTH = 128;
+
+const LABELS = [
+  "admiration",
+  "amusement",
+  "anger",
+  "annoyance",
+  "approval",
+  "caring",
+  "confusion",
+  "curiosity",
+  "desire",
+  "disappointment",
+  "disapproval",
+  "disgust",
+  "embarrassment",
+  "excitement",
+  "fear",
+  "gratitude",
+  "grief",
+  "joy",
+  "love",
+  "nervousness",
+  "optimism",
+  "pride",
+  "realization",
+  "relief",
+  "remorse",
+  "sadness",
+  "surprise",
+  "neutral",
+];
+
+// Reactive elements of the page.
+const message = ref(0);
+const initialized = ref(false);
+const userInput = ref(null);
+const verdict = ref(null);
+const fullOutput = ref(null);
+let model = null;
+
+const runInference = debounce(async () => {
+  console.log(`running inference on ${userInput.value}`);
+  let inputs = RetVec.binarizer(userInput.value, RETVEC_STR_LENGTH);
+
+  inputs = tf.expandDims(inputs, 0); // make it a batch
+  let prediction = await model.executeAsync(inputs);
+  fullOutput.value = await prediction.data();
+  const topKprediction = prediction.as1D().topk();
+  const topIndex = topKprediction.indices.dataSync()[0];
+  const topValue = topKprediction.values.dataSync()[0];
+  verdict.value = `${LABELS[topIndex]} (${(topValue * 100).toFixed(2)})`;
+}, 100);
+
+watch(userInput, async () => {
+  if (!initialized.value) return;
+  if (!userInput.value) return;
+  runInference();
+});
+
+// Load RetVec at startup.
+onMounted(async () => {
+  message.value = "Initializing RetVec...";
+  await RetVec.init(retvecModelUrl, RETVEC_ENCODING_SIZE);
+  message.value = "Loading model...";
+  model = await tf.loadGraphModel(modelUrl);
+  message.value = "RetVec ready!";
+  initialized.value = true;
+  console.log(tf.getBackend());
+  userInput.value = "I enjoy hving a g00d ic3cream!!! 🍦";
+});
+</script>
--- demos/js/src/main.js
+++ demos/js/src/main.js
@@ -3,12 +3,14 @@ import {createRouter, createWebHistory} from 'vue-router';
 
 import App from "./App.vue";
 import BinarizerDemo from "./components/BinarizerDemo.vue";
+import EmotionDemo from "./components/EmotionDemo.vue";
 
 // Load each demo on a separate route.
 const router = createRouter( {
     history: createWebHistory(),
     routes: [
         {path: '/', name: 'Home', component: BinarizerDemo},
+        {path: '/emotion_demo', name: 'EmotionDemo', component: EmotionDemo},
     ]
 });
 
