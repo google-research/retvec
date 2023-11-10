8136ccb99e1e22886d19d9831780a6f0f85fc284
Luca Invernizzi <invernizzi@google.com>
2023-11-10 14:37:20 +0000

    Adds links to multiple demos

--- demos/js/src/App.vue
+++ demos/js/src/App.vue
@@ -1,4 +1,50 @@
 <template>
-  <router-view></router-view>
+  <v-app app>
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-navigation-drawer
+      v-model="drawer"
+      rounded="xl"
+      border="none"
+      color="#f3f6fc"
+      app
+    >
+      <v-sheet color="#f3f6fc" class="pl-4">
+        <v-spacer></v-spacer>
+        <v-list :items="items"></v-list>
+      </v-sheet>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container class="py-8 px-6" fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
+<script>
+export default {
+  data: () => ({
+    selectedItem: null,
+    drawer: true,
+    items: [
+      {
+        title: "Emotion demo",
+        value: 1,
+        props: {
+          prependIcon: "mdi-emoticon-excited-outline",
+          href: "/emotion_demo",
+        },
+      },
+      {
+        title: "Binarizer demo",
+        value: 2,
+        props: {
+          prependIcon: "mdi-barcode",
+          href: "/binarizer_demo",
+        },
+      },
+    ],
+  }),
+};
+</script>
---
+++ demos/js/src/components/Demo.vue
@@ -0,0 +1,42 @@
+/* Demo boilerplate.
+ *
+ * Keeps demos to the point by hiding a little bit of complexity.
+ */
+<template>
+  <div>
+    <div class="text-h2 pb-3">{{ title }}</div>
+    <slot></slot>
+    <v-snackbar
+      :timeout="2000"
+      color="primary"
+      rounded="pill"
+      v-model="visible"
+    >
+      <span class="text-white">
+        {{ snackbar }}
+      </span>
+    </v-snackbar>
+
+    <v-progress-linear
+      :active="visible"
+      indeterminate
+      absolute
+      bottom
+      color="primary"
+    ></v-progress-linear>
+  </div>
+</template>
+
+<script setup>
+import {ref, watch, defineProps} from "vue";
+const props = defineProps(["title", "snackbar"]);
+
+const visible = ref(true);
+
+watch(
+  () => props.snackbar,
+  () => {
+    visible.value = !!props.snackbar;
+  },
+);
+</script>
